4a72750d1d6efb0fefc345bd0d1cdc61115242f7
Mengyao Huang <mengyao_huang@berkeley.edu>
2026-01-23 14:03:55 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Render is live ✅"
+
+@app.get("/chat")
+def chat():
+    rid = request.args.get("rid", "missing")
+    cond = request.args.get("cond", "missing")
+    return f"Chat page. rid={rid}, cond={cond}"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
